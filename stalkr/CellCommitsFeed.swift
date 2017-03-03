b6ed91f1be419c5510ca1db6d2b98da7a0854f4c
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-03-03 09:26:06 -0300

    feat: cell Commits Feed now consumes data from websocket

--- stalkr/CellCommitsFeed.swift
+++ stalkr/CellCommitsFeed.swift
@@ -9,8 +9,10 @@
 import UIKit
 import Kingfisher
 import SwiftRichString
+import SwiftyJSON
+import RelativeFormatter
 
-class CellCommitsFeed: SlotableCellDefault, SlotableCell, UITableViewDelegate, UITableViewDataSource {
+class CellCommitsFeed: SlotableCellDefault, SlotableCell, SubscriberCell, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var table: UITableView!
     static let cellName = "Commits Feed"
@@ -19,12 +21,43 @@ class CellCommitsFeed: SlotableCellDefault, SlotableCell, UITableViewDelegate, U
     let haveZoom = false
     let cellHeight = (UINib(nibName: "CellCommitsFeedTableCell", bundle: nil).instantiate(withOwner: nil, options: nil).last as! UIView).frame.size.height
     
-    static let configurations: [ConfigInput] = []
+    var commitsLog: [CommitRegister] = []
     
+    // config
+    static let configurations: [ConfigInput] = [
+        ConfigInput(name: "owner", label: "GitHub's user", inputType: .text, obligatory: true),
+        ConfigInput(name: "project", label: "GitHub's repository", inputType: .text, obligatory: true)
+    ]
+
+    // subscriber
+    let webSockets = [
+        WebSocketConfig(
+            requestStartUrl: "https://stalkr-api-commits-github.herokuapp.com",
+            requestStartParams: { config in
+                return ["owner": config["owner"] as! String, "project": config["project"] as! String]
+            },
+            channel: { config in
+                let owner = config["owner"] as! String
+                let project = config["project"] as! String
+                
+                return "commits-github-\(owner)-\(project)"
+            },
+            event: "status-requested"
+        )
+    ]
+    
+    let webSocketHandles: [String: (_ data: JSON, _ cell: SlotableCell) -> Void] = [
+        "status-requested": { json, cell in
+            (cell as! CellCommitsFeed).commitsLog = json["payload"].arrayValue.map { CommitRegister(json: $0) }
+            (cell as! CellCommitsFeed).table.reloadData()
+        }
+    ]
+ 
+    //
     func load(params: [String: Any]) {
         self.table.delegate = self
         self.table.dataSource = self
-        
+ 
         self.table.register(CellTrevisTableCell.self, forCellReuseIdentifier: "CellCommitsFeedTableCell")
         self.table.register(UINib(nibName: "CellCommitsFeedTableCell", bundle: nil), forCellReuseIdentifier: "CellCommitsFeedTableCell")
     }
@@ -39,17 +72,21 @@ class CellCommitsFeed: SlotableCellDefault, SlotableCell, UITableViewDelegate, U
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return commitsLog.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellCommitsFeedTableCell", for: indexPath) as! CellCommitsFeedTableCell
+        let currentCommit = commitsLog[indexPath.row]
         
-        cell.imagePhoto.kf.setImage(with: URL(string: "https://avatars0.githubusercontent.com/u/9501115"))
+        cell.imagePhoto.kf.setImage(with: URL(string: currentCommit.imageUrl))
         cell.imagePhoto.asCircle()
+        cell.labelCommitterName.text = currentCommit.name
         cell.textMessage.textColor = UIColor.fontPullMessage
-        cell.labelCommitHash.attributedText = "Commit " + "be5e5440".set(style: .fontBold)
-        cell.labelBranch.attributedText = "Branch " + "Master".set(style: .fontBold)
+        cell.textMessage.text = currentCommit.message
+        cell.labelCommitHash.attributedText = "Commit " + currentCommit.sha.set(style: .fontBold)
+        cell.labelBranch.attributedText = "Branch " + "Master".set(style: .fontBold) // TODO
+        cell.labelTimeAgo.text = currentCommit.date.relativeFormatted()
         
         return cell
     }
