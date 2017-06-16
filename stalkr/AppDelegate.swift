0e02348906bb71ce7a189321825aa0ef444fca98
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-06-16 09:27:10 -0300

    feat(Authentication screen): add Authentication.storyboard and move the logic to select the initial view controller AppDelegate

--- stalkr/AppDelegate.swift
+++ stalkr/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let initialViewController = UIStoryboard(name: "Authentication", bundle: nil).instantiateInitialViewController()!
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.rootViewController = initialViewController
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
